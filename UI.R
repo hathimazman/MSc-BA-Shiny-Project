eaf760ee2db1273bcf8bbf24d0cf20b9436f44fd
hathimazman <88342227+hathimazman@users.noreply.github.com>
2025-01-18 12:41:50 +0800

    changes

--- UI.R
+++ UI.R
@@ -1,1 +1,21 @@
-test
+ui <- 
+  fluidPage(
+    titlePanel("Barley Yield"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(inputId = "gen",  # Give the input a name "genotype"
+                    label = "1. Select genotype",  # Give the input a label to be displayed in the app
+                    choices = c("A" = "a","B" = "b","C" = "c","D" = "d","E" = "e","F" = "f","G" = "g","H" = "h"), selected = "a"),  # Create the choices that can be selected. e.g. Display "A" and link to value "a"
+        selectInput(inputId = "colour", 
+                    label = "2. Select histogram colour", 
+                    choices = c("blue","green","red","purple","grey"), selected = "grey"),
+        sliderInput(inputId = "bin", 
+                    label = "3. Select number of histogram bins", 
+                    min=1, max=25, value= c(10)),
+        textInput(inputId = "text", 
+                  label = "4. Enter some text to be displayed", "")
+      ),
+      mainPanel()
+    )
+  )
+  )
--- app.R
+++ app.R
@@ -10,10 +10,38 @@
 library(shiny)
 
 # Define UI for application
-ui <- fluidPage()
+ui <- 
+  fluidPage(
+    titlePanel("Stock Market Analysis"),
+    p("This app will allow users to analyse stock market data and provide insight on time series analysis of the selected stock"),
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons(inputId = "algorithm", 
+                     label = "1. Select preferred algorithm", 
+                     choices = c("ARIMA", "TBATS"), selected = "ARIMA"),
+        
+        dateRangeInput(inputId = 'dateRange',
+                       label = '2. Select date range',
+                       start = Sys.Date() - 2, end = Sys.Date() + 2),
+        
+        dateInput('date_input',
+                  label = '3. Select month and year to highlight',
+                  value = Sys.Date(),
+                  format = 'yyyy-mm',
+                  startview = 'year')
+      ),
+      mainPanel(
+        tabsetPanel(type = "tabs",
+                    tabPanel("Plot", plotOutput("plot")),
+                    tabPanel("Stock Summary", verbatimTextOutput("summary")),
+                    tabPanel("Time-Series Analysis", tableOutput("decompose"))
+        )
+      )
+    )
+  )
 
 # Define server logic 
-server <- function(input, output) {}
+server <- function(input, output, session) {}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
